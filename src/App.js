19d33a8cee7642d7a5e62076f3095c51ad924e1a
Tj_ <tjvishwa88@gmail.com>
2023-03-14 19:16:11 +0530

    Persistent data, learned to set up a json server and a local api and interacted with it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react'
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
 
 const App = () => {
   const [books,setBooks] = useState([]);
+  
+  const fetchBooks = async () => {
+     const response = await axios.get('http://localhost:3001/books');
+     
+     setBooks(response.data);
+  }
+
+  ///////////// NEVER DO THIS //////////////////// 
+  //fetchBooks(); // --> this calls the func again which inturn
+                // set state whenstate changes the entire dom gets called again causing an infinite loop
   
   const EditBookbyId = (id, newTitle) => {
        const updateBooks = books.map((book)=> {
@@ -23,17 +34,27 @@ const App = () => {
      setBooks(updatedBooks);
   }
 
-  const createBook = (title) => {
+  const createBook = async (title) => {
     //BAD CODE!
     // books.push({id:123, title:title});
 
     // this creates a new array so components get's rerendered in react
+    // const updateBooks = [
+    //     ...books,
+    //     {
+    //       id:Math.round(Math.random() * 999),
+    //       title}
+    // ]
+
+    // adding a book by sending a request to api
+    const response = await axios.post('http://localhost:3001/books', {
+      title
+    })
+    
     const updateBooks = [
-        ...books,
-        {
-          id:Math.round(Math.random() * 999),
-          title}
-    ]
+           ...books,
+           response.data
+          ];
     setBooks(updateBooks);
   }
   return (
