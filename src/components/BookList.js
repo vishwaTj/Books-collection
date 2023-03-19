0a94ec08c6fb81b678753faeeeac24216456aea6
Tj_ <tjvishwa88@gmail.com>
2023-03-19 15:16:35 +0530

    Removed basic Context API syntaxt started implementing the main code into Context API

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,15 +4,12 @@ import BooksContext from '../context/books';
 import BookShow from './Bookshow';
 
 const BookList = ({books, onDelete, onEdit}) => {
-  const {count, incrementCount} = useContext(BooksContext);
 
   const renderedBooks = books.map((book) =>{
     return <BookShow onDelete={onDelete} key={book.id}  book={book} onEdit={onEdit} />;
   })
   return (
     <div className='book-list'>
-      {count}
-       <button onClick={incrementCount} >click </button>
       {renderedBooks}
     </div>
   )
--- src/context/books.js
+++ src/context/books.js
@@ -2,17 +2,9 @@ import {createContext,useState} from 'react';
 const BooksContext = createContext();
 
 function Provider({ children }) {
-    const [count, setCount] = useState(5);
-
-    const valueToShare = {
-        count,
-        incrementCount: () => {
-            setCount(count + 1);
-        }
-    };
 
     return( 
-        <BooksContext.Provider value={valueToShare}>
+        <BooksContext.Provider value={{}}>
             {children}
         </BooksContext.Provider>
     );    
